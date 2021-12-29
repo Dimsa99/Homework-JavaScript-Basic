b3af94b4b08f3af84a79dd1611130ed93d2811a9
Dimsa99 <dimsajosifovski@gmail.com>
2021-12-29 20:41:10 +0100

    Update exercise5.js

--- homework/src/exercise5.js
+++ homework/src/exercise5.js
@@ -1,8 +1,9 @@
-function ageCalculator(yourYear, currentYear){
-    let yourAge = currentYear - yourYear;
+function ageCalculator(yourYear){
+    const currentYear= new Date().getFullYear();
+    const yourAge = currentYear - yourYear;
     console.log(`Current year is: ${currentYear}, your birth year is: ${yourYear} and your age is: ${yourAge}`);
 
 }
-ageCalculator(1985,2021);
-ageCalculator(1859,1915);
-ageCalculator(2000,2020);
+ageCalculator(1985);
+ageCalculator(1859);
+ageCalculator(2000);
